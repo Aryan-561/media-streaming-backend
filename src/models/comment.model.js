4374c3ea59f925edd4d129a88c310b1303c057fc
Aryan <aryanmail561@gmail.com>
2025-02-09 19:31:16 +0530

    add models for comments, likes, playlists, and tweets

---
+++ src/models/comment.model.js
@@ -0,0 +1,28 @@
+import mongoose, {Schema} from "mongoose"
+
+const commentSchema = Schema({
+
+    content:{
+        type:String,
+        required:true
+    },
+
+    video:{
+        type: Schema.Types.ObjectId,
+        ref: "Video"
+    },
+
+    owner:{
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+
+    tweet:{
+        type: Schema.Types.ObjectId(),
+        ref: "Tweet"
+    }
+
+
+},{timestamps:true})
+
+export const Comment = mongoose.model("Comment", commentSchema)
